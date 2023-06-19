4bdd9e36bb981fb7a90aa5d8f7a859f4e8ebeab2
0x4445565a <brandon@ibreak.systems>
2023-06-18 21:26:59 -0500

    Adding , and # operators

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
+use std::io::Write;
+
 const MEMORY_BUFFER_SIZE: usize = 255;
 
 fn main() {
-    let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-    println!("Program: {program}");
+    let program = ">>+++++[-<+++++>]<+<,>[-<.->]";
     run_bf_program(program);
 }
 
@@ -16,6 +18,10 @@ fn run_bf_program(program: &str) {
 
     while cursor < program.len() {
         match program[cursor] as char {
+            '#' => println!(
+                "Memory: {arr:?}\nPointer: {pointer}\nValue: {}",
+                arr[pointer]
+            ),
             '-' => arr[pointer] = arr[pointer].wrapping_sub(1),
             '+' => arr[pointer] = arr[pointer].wrapping_add(1),
             '>' => pointer = (pointer + 1) % MEMORY_BUFFER_SIZE,
@@ -30,14 +36,26 @@ fn run_bf_program(program: &str) {
             '[' => stack.push(cursor),
             ']' => {
                 let start = stack.pop().unwrap();
+                // Set the cursor back to the start of the loop
                 if arr[pointer] != 0 {
                     cursor = start;
                     continue;
                 }
             }
+            ',' => {
+                print!("Input number 0-255: ");
+                std::io::stdout()
+                    .flush()
+                    .expect("Failed to flush STDOUT buffer");
+                let mut i = String::new();
+                io::stdin().read_line(&mut i).unwrap_or(0);
+                let i: u8 = i.trim().parse().unwrap_or(0);
+                arr[pointer] = i;
+            }
             _ => (),
         };
 
+        // Advance if the loop didn't continue early
         cursor += 1;
     }
 }
