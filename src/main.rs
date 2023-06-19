168f0956d89f068a72ca320cd2fcb07de1277dbd
0x4445565a <brandon@ibreak.systems>
2023-06-18 21:04:02 -0500

    Handling the loops by using logic from the open close bracket CS question

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
 const MEMORY_BUFFER_SIZE: usize = 255;
+
 fn main() {
-    let program = "++>++->";
+    let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    println!("Program: {program}");
+    run_bf_program(program);
+}
+
+fn run_bf_program(program: &str) {
+    let program: &[u8] = program.as_bytes();
+
     let mut arr: [u8; MEMORY_BUFFER_SIZE] = [0; MEMORY_BUFFER_SIZE];
-    let mut pointer = 0;
-    for c in program.chars() {
-        match c {
-            '-' => arr[pointer] -= 1,
-            '+' => arr[pointer] += 1,
-            '>' => pointer += 1,
-            _ => println!("Skip"),
+    let mut pointer: usize = 0;
+    let mut cursor: usize = 0;
+    let mut stack: Vec<usize> = vec![];
+
+    while cursor < program.len() {
+        match program[cursor] as char {
+            '-' => arr[pointer] = arr[pointer].wrapping_sub(1),
+            '+' => arr[pointer] = arr[pointer].wrapping_add(1),
+            '>' => pointer = (pointer + 1) % MEMORY_BUFFER_SIZE,
+            '<' => {
+                pointer = if pointer == 0 {
+                    MEMORY_BUFFER_SIZE - 1
+                } else {
+                    (pointer - 1) % MEMORY_BUFFER_SIZE
+                }
+            }
+            '.' => print!("{}", arr[pointer] as char),
+            '[' => stack.push(cursor),
+            ']' => {
+                let start = stack.pop().unwrap();
+                if arr[pointer] != 0 {
+                    cursor = start;
+                    continue;
+                }
+            }
+            _ => (),
         };
+
+        cursor += 1;
     }
-    println!("{program}\n{arr:?}");
 }
